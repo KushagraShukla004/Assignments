c985573fdd316ebb2ad179814e93226692aa7f66
Kushagra_Shukla <73269482+KushagraShukla004@users.noreply.github.com>
2024-06-22 21:43:40 +0530

    Complete

---
+++ dashhboard/src/pages/index.jsx
@@ -0,0 +1,15 @@
+export { default as Ecommerce } from "./Ecommerce";
+export { default as Kanban } from "./Kanban";
+export { default as Orders } from "./Orders";
+export { default as Employees } from "./Employees";
+export { default as Editor } from "./Editor";
+export { default as Customers } from "./Customers";
+export { default as Calender } from "./Calender";
+export { default as Area } from "./pageCharts/Area";
+export { default as Bar } from "./pageCharts/Bar";
+export { default as ColorMapping } from "./pageCharts/ColorMapping";
+export { default as Financial } from "./pageCharts/Financial";
+export { default as Line } from "./pageCharts/Line";
+export { default as Pie } from "./pageCharts/Pie";
+export { default as Pyramid } from "./pageCharts/Pyramid";
+export { default as Stacked } from "./pageCharts/Stacked";
