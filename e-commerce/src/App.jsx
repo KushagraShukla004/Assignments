264c4ec364eaf9400a24abbd5e7f531144f4368d
Kushagra_Shukla <73269482+KushagraShukla004@users.noreply.github.com>
2024-07-15 15:49:08 +0530

    installed lucide-react icons

--- e-commerce/src/App.jsx
+++ e-commerce/src/App.jsx
@@ -1,6 +1,9 @@
+import { Store } from "lucide-react";
+
 const App = () => {
   return (
     <div>
+      <Store color="red" strokeWidth={2} />
       <h1 className="underline text-3xl">E-COMMERCE</h1>
     </div>
   );
