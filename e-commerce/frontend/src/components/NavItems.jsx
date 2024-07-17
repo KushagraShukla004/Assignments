ced70e388286fa28c4838c3ab6b7d75a0c108021
Kushagra_Shukla <73269482+KushagraShukla004@users.noreply.github.com>
2024-07-18 00:01:28 +0530

    Sidebar Complete

--- e-commerce/frontend/src/components/NavItems.jsx
+++ e-commerce/frontend/src/components/NavItems.jsx
@@ -2,64 +2,39 @@
 
 import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
-import { useCycle } from "framer-motion";
+// import { useCycle } from "framer-motion";
 
 {
   /* object destructuring 101:
   when you pass icon={House} to NavItem, within NavItem, Icon will refer to House. This renaming is helpful when you want to use a different name inside the component than the name of the prop passed to it. 
   */
 }
-const NavItem = ({ icon: Icon, text, link }) => {
-  const [iconHover, cycleIconHover] = useCycle(false, true);
-  const containerVariants = {
-    hover: {
-      perspective: 1000,
-      transform: "scale(1.05)",
-      transition: { duration: 0.3, ease: "easeInOut" },
-    },
-    rest: {
-      perspective: 1000,
-      transform: "scale(1)",
-      transition: { duration: 0.3, ease: "easeInOut" },
-    },
-  };
 
-  const iconVariants = {
+const NavItem = ({ icon: Icon, text, link, sidebarHover }) => {
+  const textVariants = {
     open: {
       opacity: 1,
       display: "block",
-      transition: { duration: 0.2, ease: "easeInOut" },
+      transition: { delay: 0.2, duration: 0.2, ease: "easeInOut" },
     },
     closed: {
       opacity: 0,
       display: "none",
-      transition: { duration: 0.2, ease: "easeInOut" },
+      transition: { delay: 0.2, duration: 0.2, ease: "easeInOut" },
     },
   };
 
   return (
-    <motion.div
-      variants={containerVariants}
-      initial="rest"
-      whileHover="hover"
-      onHoverStart={() => cycleIconHover()}
-      onHoverEnd={() => cycleIconHover()}
-    >
+    <motion.div className="flex items-center">
       <Link
         to={link}
         className="flex transform cursor-pointer items-center hover:w-[20rem] hover:rounded-l-xl hover:bg-fuchsia-600"
       >
-        {Icon && (
-          <Icon
-            size={40}
-            className="px-2"
-            strokeWidth={iconHover ? "2" : "1"}
-          />
-        )}
+        {Icon && <Icon size={40} className="px-2" />}
 
-        {iconHover && (
+        {sidebarHover && (
           <motion.span
-            variants={iconVariants}
+            variants={textVariants}
             initial="closed"
             animate="open"
             className="px-2 font-bold tracking-widest"
--- e-commerce/frontend/src/pages/Auth/Navigation.jsx
+++ e-commerce/frontend/src/pages/Auth/Navigation.jsx
@@ -17,11 +17,14 @@ import {
 } from "lucide-react";
 import { useCycle } from "framer-motion";
 import NavItem from "../../components/NavItems";
+import { useSelector, useDispatch } from "react-redux";
+import { useLogoutMutation } from "../../redux/api/userApiSlice";
+import { logout } from "../../redux/features/auth/authSlice";
 
 const Navigation = () => {
+  const { userInfo } = useSelector((state) => state.auth);
   const [dropdownOpen, setDropdownOpen] = useState(false);
   const [showSidebar, setShowSidebar] = useState(false);
-  const [iconHover, cycleIconHover] = useCycle(false, true);
   const [sidebarHover, cycleSidebarHover] = useCycle(false, true);
 
   const toggleDropdown = () => {
@@ -31,17 +34,44 @@ const Navigation = () => {
   const sidebarVariants = {
     open: {
       width: "15rem",
-      transition: { duration: 0.3, ease: "easeInOut" },
+      transition: { duration: 0.3, ease: "easeInOut", staggerChildren: 0.25 },
     },
     closed: {
       width: "5rem",
-      transition: { duration: 0.3, ease: "easeInOut" },
+      transition: { duration: 0.3, ease: "easeInOut", staggerChildren: 0.25 },
     },
   };
 
+  const textVariants = {
+    open: {
+      opacity: 1,
+      display: "block",
+      transition: { delay: 0.2, duration: 0.2, ease: "easeInOut" },
+    },
+    closed: {
+      opacity: 0,
+      display: "none",
+      transition: { delay: 0.2, duration: 0.2, ease: "easeInOut" },
+    },
+  };
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [logoutApiCall] = useLogoutMutation();
+
+  const logoutHandler = async () => {
+    try {
+      await logoutApiCall().unwrap();
+      dispatch(logout());
+      navigate("/login");
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
     <motion.div
-      className={`z-[999] ${showSidebar ? "hidden" : "flex"} flex h-screen w-[10%] flex-col justify-between overflow-hidden rounded-r-3xl bg-zinc-800 p-4 text-white max-phone:hidden`}
+      className={`z-[9999] ${showSidebar ? "hidden" : "flex"} fixed flex h-screen w-[10%] flex-col justify-between overflow-hidden rounded-r-3xl bg-zinc-800 p-4 text-white max-phone:hidden`}
       variants={sidebarVariants}
       initial="closed"
       whileHover="open"
@@ -53,32 +83,73 @@ const Navigation = () => {
           <motion.img
             src={LogoFilled}
             alt="Logo1"
-            //trick: use invert to change a black image into white or vice versa
             className="z-[999] size-10 invert"
           />
 
           {sidebarHover && (
             <motion.h1
-              initial={{ opacity: 0, display: "none" }}
-              animate={{ opacity: 1, display: "block" }}
-              transition={{ delay: 0.2, duration: 0.2, ease: "easeInOut" }}
-              className={`px-2 text-3xl tracking-wide ${sidebarHover && "block"} hover:max-sm:hidden`}
-              style={{ display: sidebarHover ? "block" : "none" }}
+              variants={textVariants}
+              initial="closed"
+              animate="open"
+              className={`px-2 text-3xl tracking-wide hover:max-sm:hidden`}
             >
               Shopify
             </motion.h1>
           )}
         </motion.div>
-        <NavItem icon={House} text="HOME" link="/" />
-        <NavItem icon={Store} text="SHOP" link="/shop" />
-        <NavItem icon={ShoppingCart} text="CART" link="/cart" />
-        <NavItem icon={Heart} text="Favorites" link="/favorite" />
+        <NavItem
+          icon={House}
+          text="HOME"
+          link="/"
+          sidebarHover={sidebarHover}
+        />
+        <NavItem
+          icon={Store}
+          text="SHOP"
+          link="/shop"
+          sidebarHover={sidebarHover}
+        />
+        <NavItem
+          icon={ShoppingCart}
+          text="CART"
+          link="/cart"
+          sidebarHover={sidebarHover}
+        />
+        <NavItem
+          icon={Heart}
+          text="Favorites"
+          link="/favorite"
+          sidebarHover={sidebarHover}
+        />
+      </div>
+
+      <div className="relative">
+        <button
+          onClick={toggleDropdown}
+          className="flex items-center text-gray-800 focus:outline-none"
+        >
+          {userInfo ? (
+            <span className="text-white">{userInfo.username}</span>
+          ) : (
+            <></>
+          )}
+        </button>
       </div>
 
       <ul>
         <li>
-          <NavItem icon={LogIn} text="LogIn" link="/login" />
-          <NavItem icon={UserRoundPlus} text="Register" link="/register" />
+          <NavItem
+            icon={LogIn}
+            text="LogIn"
+            link="/login"
+            sidebarHover={sidebarHover}
+          />
+          <NavItem
+            icon={UserRoundPlus}
+            text="Register"
+            link="/register"
+            sidebarHover={sidebarHover}
+          />
         </li>
       </ul>
     </motion.div>
