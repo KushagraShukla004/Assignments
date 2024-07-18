c77764986d042b1d631b18ce2fc7035963621f7c
Kushagra_Shukla <73269482+KushagraShukla004@users.noreply.github.com>
2024-07-18 23:38:50 +0530

    userApi endpoints completed

--- e-commerce/frontend/src/redux/api/userApiSlice.js
+++ e-commerce/frontend/src/redux/api/userApiSlice.js
@@ -23,6 +23,40 @@ export const userApiSlice = apiSlice.injectEndpoints({
         body: data,
       }),
     }),
+    profile: builder.mutation({
+      query: (data) => ({
+        url: `${USERS_URL}/profile`,
+        method: "PUT",
+        body: data,
+      }),
+    }),
+    getUsers: builder.query({
+      query: () => ({
+        url: USERS_URL,
+      }),
+      providesTags: ["User"],
+      keepUnusedDataFor: 5,
+    }),
+    deleteUser: builder.mutation({
+      query: (userId) => ({
+        url: `${USERS_URL}/${userId}`,
+        method: "DELETE",
+      }),
+    }),
+    getUserDetails: builder.query({
+      query: (id) => ({
+        url: `${USERS_URL}/${id}`,
+      }),
+      keepUnusedDataFor: 5,
+    }),
+    updateUser: builder.mutation({
+      query: (data) => ({
+        url: `${USERS_URL}/${data.userId}`,
+        method: "PUT",
+        body: data,
+      }),
+      invalidatesTags: ["User"],
+    }),
   }),
 });
 
@@ -30,9 +64,9 @@ export const {
   useLoginMutation,
   useLogoutMutation,
   useRegisterMutation,
-  // useProfileMutation,
-  // useGetUsersQuery,
-  // useDeleteUserMutation,
-  // useUpdateUserMutation,
-  // useGetUserDetailsQuery,
+  useProfileMutation,
+  useGetUsersQuery,
+  useDeleteUserMutation,
+  useUpdateUserMutation,
+  useGetUserDetailsQuery,
 } = userApiSlice;
