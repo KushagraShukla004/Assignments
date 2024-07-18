dde686e5c5429e58f546aa4de4cc9141b2fe0694
Kushagra_Shukla <73269482+KushagraShukla004@users.noreply.github.com>
2024-07-18 11:52:38 +0530

    Added Routes to main

--- e-commerce/frontend/src/main.jsx
+++ e-commerce/frontend/src/main.jsx
@@ -3,10 +3,21 @@ import App from "./App.jsx";
 import "./index.css";
 import { Route, RouterProvider, createRoutesFromElements } from "react-router";
 import { createBrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./redux/store.js";
+
+//Auth
+import Login from "./pages/Auth/Login.jsx";
 
 const router = createBrowserRouter(
-  createRoutesFromElements(<Route path="/" element={<App />} />),
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route path="/login" element={<Login />} />
+    </Route>,
+  ),
 );
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <RouterProvider router={router} />,
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>,
 );
