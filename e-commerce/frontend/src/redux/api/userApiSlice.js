698c29754530f790f1bf4961cc0098397e5532f1
Kushagra_Shukla <73269482+KushagraShukla004@users.noreply.github.com>
2024-07-18 11:52:18 +0530

    Redux for auth and userApi is created

---
+++ e-commerce/frontend/src/redux/api/userApiSlice.js
@@ -0,0 +1,38 @@
+import { apiSlice } from "./apiSlice";
+import { USERS_URL } from "../constants";
+
+export const userApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    login: builder.mutation({
+      query: (data) => ({
+        url: `${USERS_URL}/auth`,
+        method: "POST",
+        body: data,
+      }),
+    }),
+    logout: builder.mutation({
+      query: () => ({
+        url: `${USERS_URL}/logout`,
+        method: "POST",
+      }),
+    }),
+    register: builder.mutation({
+      query: (data) => ({
+        url: `${USERS_URL}`,
+        method: "POST",
+        body: data,
+      }),
+    }),
+  }),
+});
+
+export const {
+  useLoginMutation,
+  useLogoutMutation,
+  useRegisterMutation,
+  // useProfileMutation,
+  // useGetUsersQuery,
+  // useDeleteUserMutation,
+  // useUpdateUserMutation,
+  // useGetUserDetailsQuery,
+} = userApiSlice;
---
+++ e-commerce/frontend/src/redux/constants.js
@@ -0,0 +1,2 @@
+export const BASE_URL = ""; //proxy
+export const USERS_URL = "/api/users";
