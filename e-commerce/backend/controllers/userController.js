9f14af204e8c7511fbdb030ae73b4f77a7d54e21
Kushagra_Shukla <73269482+KushagraShukla004@users.noreply.github.com>
2024-07-18 23:14:39 +0530

    Fixed bug in backend api for login

--- e-commerce/backend/controllers/userController.js
+++ e-commerce/backend/controllers/userController.js
@@ -41,23 +41,23 @@ const createUser = asyncHandler(async (req, res) => {
 
 const loginUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
-
   const existingUser = await User.findOne({ email });
 
   if (existingUser) {
     const isPasswordValid = await bcrypt.compare(password, existingUser.password);
-
     if (isPasswordValid) {
       createToken(res, existingUser._id);
-
-      res.status(201).json({
+      res.status(200).json({
         _id: existingUser._id,
         username: existingUser.username,
         email: existingUser.email,
         isAdmin: existingUser.isAdmin,
       });
-      return;
+    } else {
+      res.status(401).json({ message: "Invalid password" });
     }
+  } else {
+    res.status(404).json({ message: "User not found" });
   }
 });
 
