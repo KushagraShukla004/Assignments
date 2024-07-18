9d6172a8e5fe819fc6de2f0660b264f8c88f918f
Kushagra_Shukla <73269482+KushagraShukla004@users.noreply.github.com>
2024-07-18 23:20:34 +0530

    minor change

--- e-commerce/frontend/src/components/Loader.jsx
+++ e-commerce/frontend/src/components/Loader.jsx
@@ -1,6 +1,6 @@
 const Loader = () => {
   return (
-    <div className="h-16 w-16 animate-spin rounded-full border-t-4 border-[#9748FF] border-opacity-50"></div>
+    <div className="mt-2 h-16 w-16 animate-spin rounded-full border-t-4 border-[#9748FF] border-opacity-50"></div>
   );
 };
 
