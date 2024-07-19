f0c2710b8c98fab44a78e6e01c6abe355e94d818
Kushagra_Shukla <73269482+KushagraShukla004@users.noreply.github.com>
2024-07-19 19:50:55 +0530

    Product CRUD operations completed

--- e-commerce/backend/index.js
+++ e-commerce/backend/index.js
@@ -5,6 +5,7 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import userRoutes from "./routes/userRoutes.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
 //Utils
 import connectDB from "./config/db.js";
 
@@ -21,6 +22,7 @@ app.use(cookieParser());
 
 app.use("/api/users", userRoutes);
 app.use("/api/category", categoryRoutes);
+app.use("/api/products", productRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
