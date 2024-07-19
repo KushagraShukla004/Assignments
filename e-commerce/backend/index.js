d78c4aa1d4d2bafb2e6cce1ad2918967260f4115
Kushagra_Shukla <73269482+KushagraShukla004@users.noreply.github.com>
2024-07-19 12:38:53 +0530

    Category Api backend completed

--- e-commerce/backend/index.js
+++ e-commerce/backend/index.js
@@ -4,6 +4,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import userRoutes from "./routes/userRoutes.js";
+import categoryRoutes from "./routes/categoryRoutes.js";
 //Utils
 import connectDB from "./config/db.js";
 
@@ -19,6 +20,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use("/api/users", userRoutes);
+app.use("/api/category", categoryRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
---
+++ e-commerce/backend/routes/categoryRoutes.js
@@ -0,0 +1,21 @@
+import express from "express";
+const router = express.Router();
+import {
+  createCategory,
+  updateCategory,
+  removeCategory,
+  listCategory,
+  readCategory,
+} from "../controllers/categoryController.js";
+
+import { authenticate, authorizeAdmin } from "../middlewares/authMiddleware.js";
+
+//route is {url}/api/category/
+router.route("/").post(authenticate, authorizeAdmin, createCategory);
+router.route("/:categoryId").put(authenticate, authorizeAdmin, updateCategory);
+router.route("/:categoryId").delete(authenticate, authorizeAdmin, removeCategory);
+
+router.route("/categories").get(listCategory);
+router.route("/:id").get(readCategory);
+
+export default router;
