bdfb79d317752dd1becb2a2c2cadabb1c33554d2
Kushagra_Shukla <73269482+KushagraShukla004@users.noreply.github.com>
2024-07-19 20:37:02 +0530

    Image upload Api completed

--- e-commerce/backend/index.js
+++ e-commerce/backend/index.js
@@ -1,12 +1,14 @@
-// imports
 import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import fs from "fs";
 import userRoutes from "./routes/userRoutes.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
-//Utils
+import uploadRoutes from "./routes/uploadRoutes.js";
+
+// Utils
 import connectDB from "./config/db.js";
 
 dotenv.config();
@@ -20,9 +22,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+// Check if uploads directory exists, create if not
+const __dirname = path.resolve();
+const uploadDir = path.join(__dirname, "uploads");
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir);
+}
+
 app.use("/api/users", userRoutes);
 app.use("/api/category", categoryRoutes);
 app.use("/api/products", productRoutes);
+app.use("/api/upload", uploadRoutes);
+
+app.use("/uploads", express.static(uploadDir));
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
