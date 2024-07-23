864211bede4b23dd307a0e3a6af55b750e4c541c
Kushagra_Shukla <73269482+KushagraShukla004@users.noreply.github.com>
2024-07-23 14:01:30 +0530

    Favorites Feature created

--- e-commerce/frontend/src/redux/constants.js
+++ e-commerce/frontend/src/redux/constants.js
@@ -3,3 +3,4 @@ export const USERS_URL = "/api/users";
 export const CATEGORY_URL = "/api/category";
 export const PRODUCT_URL = "/api/products";
 export const UPLOAD_URL = "/api/upload";
+export const ORDERS_URL = "/api/orders";
