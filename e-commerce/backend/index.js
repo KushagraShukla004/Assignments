8c05b3932201c5af417aaf081447b54a5afd698e
Kushagra_Shukla <73269482+KushagraShukla004@users.noreply.github.com>
2024-07-26 19:50:43 +0530

    Orders Backend Completed

--- e-commerce/backend/index.js
+++ e-commerce/backend/index.js
@@ -7,6 +7,7 @@ import userRoutes from "./routes/userRoutes.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import uploadRoutes from "./routes/uploadRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
 
 // Utils
 import connectDB from "./config/db.js";
@@ -33,6 +34,7 @@ app.use("/api/users", userRoutes);
 app.use("/api/category", categoryRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/upload", uploadRoutes);
+app.use("/api/orders", orderRoutes);
 
 app.use("/uploads", express.static(uploadDir));
 
