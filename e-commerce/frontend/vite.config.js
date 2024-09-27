35faef169c76bd04ce26ab12e7a2f67acf595f64
Kushagra_Shukla <73269482+KushagraShukla004@users.noreply.github.com>
2024-09-27 23:15:44 +0530

    proxy server

--- e-commerce/frontend/vite.config.js
+++ e-commerce/frontend/vite.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api/": "http://localhost:5000",
-      "/uploads/": "http://localhost:5000",
+      "/api/": "https://shopifyi.netlify.app/",
+      "/uploads/": "https://shopifyi.netlify.app/",
     },
   },
 });
